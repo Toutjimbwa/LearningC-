e7b47e7dca8a5434ecfe482e5ba26508a43bb2ae
Tistou Blomberg <tistou.blomberg@gmail.com>
2020-09-15 01:21:57 +0200

    Vocabulary and Hotkey Format

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; //Library or Namespace
+
+//Formating the document properly
+//Edit/Advanced/Format Document
+//Or CTRL + K, CTRL + D (Both in sequence).
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] foShizzleMaNizzle)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
