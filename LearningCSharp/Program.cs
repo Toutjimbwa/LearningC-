cf358213f07fa22090f7706e1129da8bfabd01c4
Tistou Blomberg <tistou.blomberg@gmail.com>
2020-09-15 01:33:38 +0200

    For and tab twice

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 
-//CTRL+C to stop a program.
-
 namespace LearningCSharp
 {
     class Program
     {
         static void Main(string[] foShizzleMaNizzle)
         {
-            int counter = 0;
-            while (counter < 10)
+
+            //Write For and press Tab x2
+            //Then tab to switch between counter variable name and less than variable name or number.
+            int limit = 10;
+            for (int s = 0; s < limit; s++)
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine(s);
             }
+
+            //tab twice info can also be seen when writing if statements
         }
     }
 }
